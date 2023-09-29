42595b31a7a3a5cc6e6387fa30e5995a1bb96191
cesarcuadro <cesarcuadro777@gmail.com>
2023-09-29 02:17:02 -0700

    implemented navbar. next is adding route to button

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,8 @@ import ErrorPage from "./routes/error-page.jsx";
 import Posts from "./routes/Posts.jsx";
 import Tracker from "./routes/Tracker.jsx";
 import Lists from "./routes/Lists.jsx";
+import { StyledEngineProvider } from "@mui/material";
+import ResponsiveAppBar from "./components/navbar.jsx";
 
 const router = createBrowserRouter([
   {
@@ -29,6 +31,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <StyledEngineProvider injectFirst>
+      <ResponsiveAppBar />
+      <RouterProvider router={router} />
+    </StyledEngineProvider>
   </React.StrictMode>
 );
